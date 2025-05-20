6946392a738dfe9ba4b445e6eec526b1b8e7be72
Oleksandr Zaikin <nuh.ubf@gmail.com>
2025-05-20 08:59:57 +0300

    Cyrillic highlighting fixed

--- src/components/misc/HighlightedText.tsx
+++ src/components/misc/HighlightedText.tsx
@@ -2,34 +2,20 @@ import React, {useMemo} from 'react';
 
 function getHighlightedText(text: string, highlights: string[], highlightWholeWords) {
   const hlower = highlights.map(s => s.toLowerCase());
-  if (highlightWholeWords) {
-    // Split text on whitespace, include term itself into parts, ignore case
-    const parts = text.split(/\b|(?=\s)|(?<=\s)/);
-    console.log({ hlower, parts, text })
-    return <>
-      {
-        parts.map(
-          (part, i) => (
-            <React.Fragment key={i}>
-            {
-              hlower.includes(part.toLowerCase())
-                ? <span className='highlighted'>{part}</span>
-                : part
-            }
-            </React.Fragment>)
-        )
-      }
-    </>;
-  }
-  // Split text on highlight term, include term itself into parts, ignore case
-  const parts = text.split(new RegExp(`(${highlights.join('|')})`, 'gi'));
+  // Split text on whitespace, include term itself into parts, ignore case
+  const parts = highlightWholeWords
+    ? text.split(/\b|(?=\s)|(?<=\s)|(?=[,.?!"])|(?<=[,.?!"])/gi)
+    : text.split(new RegExp(`(${highlights.join('|')})`, 'gi'));
+  console.log({ hlower, parts, text })
   return <>
     {
       parts.map(
         (part, i) => (
           <React.Fragment key={i}>
           {
-            hlower.includes(part.toLowerCase()) ? <span className='highlighted'>{part}</span> : part
+            hlower.includes(part.toLowerCase())
+              ? <span className='highlighted'>{part}</span>
+              : part
           }
           </React.Fragment>)
       )
